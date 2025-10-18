a8a9ba2ec3247da1aca552734fc9bcf8aa8219d4
DakshaPatil <dakshap0205@gmail.com>
2025-10-18 19:42:21 +0530

    Added new files

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -5,18 +5,30 @@ const App = ( ) => (
         className="xl:padding-1 wide:padding-r padding-1">
           Hero
         </section>
-        <section className ="padding">
+        <section className ="padding mb-6">
           PopularProducts
         </section>
-        <section className ="padding">
+        <section className ="padding mb-6">
           SuperQuality
         </section>
-        <section className ="padding-x py-10">
+        <section className ="padding-x py-10 mb-6">
           Services
         </section>
-         <section className ="padding">
+         <section className ="padding mb-6">
           SpecialOffers
         </section>
+         <section className ="bg-pale-blue padding mb-6">
+          CustomerReviews
+        </section>
+         <section className ="padding-x sm:py-32 py-16 w-full mb-6">
+          Subscribe
+        </section>
+         <section className ="bg-black padding-x padding-t pb-8 mb-6">
+          Footer
+        </section>
+
+
+
 
 
     
